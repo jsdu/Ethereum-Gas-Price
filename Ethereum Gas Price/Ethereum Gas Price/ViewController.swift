ad02ccf5a87f89264f1593985e15d386ad1b7d03
Jason Du <jsdu@uwaterloo.ca>
2020-01-18 16:25:35 -0500

    Initialize CocoaPods and add Charts

---
+++ Ethereum Gas Price/Ethereum Gas Price/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ethereum Gas Price
+//
+//  Created by Jing Song Du on 2020-01-18.
+//  Copyright © 2020 Jing Song Du. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ethereum Gas Price/Pods/Target Support Files/Pods-Ethereum Gas Price/Pods-Ethereum Gas Price-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ethereum_Gas_Price : NSObject
+@end
+@implementation PodsDummy_Pods_Ethereum_Gas_Price
+@end
